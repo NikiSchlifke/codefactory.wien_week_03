cd88ef7cbda6266735ad4955539ac1f984d56910
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-02 15:55:30 +0100

    intermedite exerc

--- day13/intermediate2/products.js
+++ day13/intermediate2/products.js
@@ -19,3 +19,34 @@ for such a purpose data-type HTML 5 attribute can be very handy.
 Try firstly to access the element using plain CSS, the accessing logic could be the same for jQuery as well.
 */
 
+// get all products on the page
+var products = $(".prod-info-main");
+// product images
+var productImages = $(products).find($("img"));
+// get specifc product kind images
+notebookImages = $(products).find('img[src^="notebook"]');
+phoneImages = $(products).find('img[src^="phone"]');
+tabletImages = $(products).find('img[src^="tablet"]');
+// get specifc products of some kind
+notebookContainers = notebookImages.parents(".prod-info-main");
+phoneContainers = phoneImages.parents(".prod-info-main");
+tabletContainers = tabletImages.parents(".prod-info-main");
+// set background colors
+notebookContainers.find($('.col-md-7, .col-md-5')).children('*').css("background-color", "red");
+phoneContainers.find($('.col-md-7, .col-md-5')).children('*').css("background-color", "green");
+tabletContainers.find($('.col-md-7, .col-md-5')).children('*').css("background-color", "blue");
+
+
+function newContainer(container, image_file, product_name, category) {
+	var template = container.first().clone();
+	template.find('img').attr('src', image_file);
+	var link = template.find('.name a');
+	link.text(product_name);
+	link.append($('<span>').text(category));
+	template.insertAfter(container.last());
+}
+
+newContainer(phoneContainers, "phone_modded.png", "Modded Phone", "Phones");
+newContainer(tabletContainers, "tablet_modded.png", "Modded Tablet", "Tablets");
+
+
