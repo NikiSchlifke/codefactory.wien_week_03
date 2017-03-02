5cf3bdb46b3f26c959b8a5b09dd0cf94d33ba941
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-03-02 13:33:06 +0100

    finish intermediate1, start 2

--- day13/intermediate1/myscript.js
+++ day13/intermediate1/myscript.js
@@ -1,20 +1,36 @@
 $("form").submit(function(e) {
 
-	e.preventDefault();
-	
-	
-	
+    e.preventDefault();
+
+
+
 
 });
 // Create jQuery selectors that will select the elements according to the requirements bellow and for each of them just change their background color to yellow:
 
 
-// Remove the form from the page
-$("#myform").remove();
+
 // Create 5 new DIVs in the article element. Each must use the text in the msg input field
 
-$("article:first()").append(
-	$('#msg').wrap('<div class="extra-wrapper"></div>');
-);
+for (var i = 0; i < 5; i++) {
+    $("article").append(
+        '<div>' + $('#msg').val() + '</div>'
+    );
+}
+
+// Remove the form from the page
+$("#myform").remove();
+
 // Add a class name of "box" to each new DIV
+// 
+$("article div").addClass('box');	
 // Move 5 elements using offset to the left positions 100, 200, 300, 400 and 500 respectively
+
+// for (var index=0 ; index<$('.box').length; index++) {
+// 	var elem = $('.box')[index];
+// 	$(elem).offset({left: 100+index*100});
+// }
+
+$('.box').map(function(index, elem) {
+	return $(elem).offset({left: 100+index*100});
+});
---
+++ day13/intermediate2/products.js
@@ -0,0 +1,21 @@
+// products.js
+
+/*
+First open the products_tempalte.html file from exercise files for this topic. 
+Go through the HTML code  in order to get more familiar with the document structure. 
+Once you understand it, write dynamic code inside myscript.js that 
+dynamically changes the background color depending on the product's type. 
+There are three types phone, notebook and tablet.
+
+For Computers use red color
+For Mobile Phones use green color
+For Tablets use blue
+After you successful dynamic styling of the products, according to their category, 
+add another two products using the same structure. One product should be the phone another the tablet.
+
+Hints: Put more attention how classes are nested one inside another, so you can access the element. 
+We have learned a lot techniques how to select elements o the page, 
+for such a purpose data-type HTML 5 attribute can be very handy.
+Try firstly to access the element using plain CSS, the accessing logic could be the same for jQuery as well.
+*/
+
